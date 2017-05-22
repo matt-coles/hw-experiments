28188be241f12e3550a2f08872d37f2d37526046
Matt Coles <mattc65@gmail.com>
2017-05-22 20:27:31 +0100

    Initial commit, adds a simple FIFO and a naive and low coverage testbench.

---
+++ fifo_tb.v
@@ -0,0 +1,104 @@
+module fifo_tb;
+
+reg reset_n;
+reg [15:0] data_in;
+wire [15:0] data_out;
+wire full, empty;
+reg fifo_push, fifo_pop, clk;
+
+simple_fifo dut (
+  .clk(clk),
+  .reset_n(reset_n),
+  .push(fifo_push),
+  .pop(fifo_pop),
+
+  .data_in(data_in),
+  .data_out(data_out),
+
+  .full(full),
+  .empty(empty)
+);
+
+initial begin
+  clk = 1'b0;
+  reset_n = 1'b1;
+  repeat(4) #10 clk = ~clk;
+  reset_n = 1'b0;
+  forever #10 clk = ~clk; // generate a clock
+end
+
+reg [15:0] tmp;
+initial begin
+  data_in = 16'h0; // initial value
+  @(negedge reset_n); // wait for reset
+  data_in = 16'hffff;
+  repeat (8) begin
+    @(posedge clk);
+    fifo_push = 1'b1;
+    @(negedge clk);
+    fifo_push = 1'b0;
+    data_in = ~data_in;
+  end
+  if (full != 1 && empty != 0) begin
+    $display("Test failed!");
+  end
+  tmp = 16'hffff;
+  repeat (8) begin
+    @(posedge clk);
+    fifo_pop = 1'b1;
+    @(negedge clk);
+    fifo_pop = 1'b0;
+    if (data_out !== tmp) begin
+      $display("Test failed!");
+    end
+    tmp = ~tmp;
+  end
+  if (full != 0 && empty != 1) begin
+    $display("Test failed!");
+  end
+  repeat(5) @(posedge clk); //if this delay is odd it fails wtf?
+  data_in = 16'h1234;
+  repeat (8) begin
+    @(posedge clk);
+    fifo_push = 1'b1;
+    @(negedge clk);
+    fifo_push = 1'b0;
+    data_in = ~data_in;
+  end
+  if (full != 1 && empty != 0) begin
+    $display("Test failed!");
+  end
+  tmp = 16'h1234;
+  repeat (8) begin
+    @(posedge clk);
+    fifo_pop = 1'b1;
+    @(negedge clk);
+    fifo_pop = 1'b0;
+    if (data_out !== tmp) begin
+      $display("Test failed!");
+    end
+    tmp = ~tmp;
+  end
+  if (full != 0 && empty != 1) begin
+    $display("Test failed!");
+  end
+  $finish;
+end
+
+initial begin
+  if ($test$plusargs("waves=1") != 0) begin
+    $dumpfile("out/waves.vcd");
+    $dumpvars(0, fifo_tb);
+    if ($test$plusargs("dump_fifo=1") != 0) begin
+      $dumpvars(0, dut.data[0]);
+      $dumpvars(0, dut.data[1]);
+      $dumpvars(0, dut.data[2]);
+      $dumpvars(0, dut.data[3]);
+      $dumpvars(0, dut.data[4]);
+      $dumpvars(0, dut.data[5]);
+      $dumpvars(0, dut.data[6]);
+      $dumpvars(0, dut.data[7]);
+    end
+  end
+end
+endmodule
---
+++ simple_fifo.v
@@ -0,0 +1,59 @@
+/*
+* DEPTH must be a power of 2
+*/
+module simple_fifo #(
+  parameter DEPTH = 8,
+  parameter WIDTH = 16
+)(
+  input wire clk,
+  input wire reset_n,
+  input wire push,
+  input wire pop,
+
+  input wire [WIDTH-1:0] data_in,
+  output reg [WIDTH-1:0] data_out,
+
+  output reg full,
+  output reg empty
+);
+
+reg [$clog2(DEPTH)-1:0] rd_ptr;
+reg [$clog2(DEPTH)-1:0] wr_ptr;
+reg [WIDTH-1:0] data [DEPTH-1:0];
+
+always @(posedge clk) begin
+  if (reset_n) begin
+    rd_ptr <= 0;
+    wr_ptr <= 0;
+    full <= 0;
+    empty <= 1;
+  end
+  else begin
+    if (push & ~full) begin
+      data[wr_ptr] <= data_in;
+      wr_ptr = wr_ptr + 1;
+      if (wr_ptr == rd_ptr) begin
+        full <= 1'b1;
+        empty <= 1'b0;
+      end
+      else begin
+        full <= 1'b0;
+        empty <= 1'b0;
+      end
+    end
+    if (pop & ~empty) begin
+      data_out <= data[rd_ptr];
+      rd_ptr = rd_ptr + 1;
+      if (wr_ptr == rd_ptr) begin
+        full <= 1'b0;
+        empty <= 1'b1;
+      end
+      else begin
+        full <= 1'b0;
+        empty <= 1'b0;
+      end
+    end
+  end
+end
+
+endmodule
